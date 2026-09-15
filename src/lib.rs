9028ce80de5292f42d99ba3db8680594fe086299
Fulminix Systems <mpratt@fulminixsystems.com>
2026-09-14 21:05:55 -0500

    Add copyright notice and licensing information

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+// =========================================================================
+// Copyright (c) 2026 Marcus Pratt. All Rights Reserved.
+// Distributed under the terms of the GNU Affero General Public License (AGPLv3).
+// For commercial production licensing inquiries, contact the author directly.
+// =========================================================================
 use std::collections::HashMap;
 use std::sync::Arc;
 use tokio::sync::{mpsc, RwLock};
